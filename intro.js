87d570f1766955d1dd073b4e872e80bc4862f091
Alejandra Lopez <mlopezalejandra13@gmail.com>
2023-08-11 22:06:21 -0500

    ejempplos para practicar

---
+++ intro.js
@@ -0,0 +1,53 @@
+let nombre = 'Alejandra'
+const boolean = false;
+let edad = 22
+
+let v1 = 2;
+let v2 = "string"
+let v3 = []
+var edad = 35
+
+console.log (v3[0])
+console.log (edad)
+
+const constante = true
+
+console.log (constante)
+
+constante = false
+console.log(constante)
+
+//COMPARACIÓN
+
+let comparacion = "2" <2;
+
+console.log (comparacion)
+
+// //MODULO
+
+let comparacion1 = 10%3;
+
+console.log (comparacion1)
+
+// para adicionar
+
+let operadores = 6;
+// operadores +=3
+// operadores -=3
+// console.log (operadores)
+// operadores *=3
+operadores /=5
+console.log (operadores)
+
+// console.log (operadores)
+let comparacion = false
+console.log(!comparacion)
+
+let miEdad 
+miEdad = 22
+let miNombre 
+miNombre = 'Alejandra'
+miEdad = (miEdad / 2) + (3 *miNombre.length)
+console.log (miEdad)
+console.log (miNombre)
+
